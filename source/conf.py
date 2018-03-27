0eac640d96b7d58a83084f29e78843bde84f0282
Jared Dillard <jared.dillard+github@gmail.com>
2018-03-27 13:37:23 -0500

    try html_context in conf

--- source/conf.py
+++ source/conf.py
@@ -88,8 +88,11 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom themes here, relative to this directory.
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
-html_theme_options = {
-    'githuburl': 'https://github.com/jdillard/continuous-sphinx/',
+html_context = {
+  'display_github': True,
+  'github_user': 'jdillard',
+  'github_repo': 'continuous-sphinx',
+  'github_version': 'master/source/'
 }
 
 # The name of an image file (within the static path) to use as favicon of the
