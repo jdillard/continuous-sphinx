7554f793ce461f9ab529c0ab04646bf4718ac83a
Jared Dillard <jdillard@netgate.com>
2018-06-17 13:02:05 -0500

    add localization

--- source/conf.py
+++ source/conf.py
@@ -66,7 +66,9 @@ release = '1.0.0'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
+
+locale_dirs = ['../l10n/locales/']
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
