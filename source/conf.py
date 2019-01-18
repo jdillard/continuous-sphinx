f7e3e3746603997c7bf5d65b70aa8cd22363be04
Jared Dillard <jdillard@netgate.com>
2019-01-17 18:58:25 -0800

    change site_url to html_baseurl

--- source/conf.py
+++ source/conf.py
@@ -27,8 +27,7 @@ sys.path.insert(0, os.path.abspath('.'))
 
 # -- General configuration ------------------------------------------------
 
-# Site base url
-site_url = 'https://continuous-sphinx.netlify.com/'
+html_baseurl = 'https://continuous-sphinx.netlify.com/'
 
 # Absolute path of conf.py
 conf_abs_path = os.path.abspath(__file__)
